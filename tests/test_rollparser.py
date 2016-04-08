852c50a8d0b09e90068061194fd7db827fbf7376
Jesse Morgan (Morgajel) <morgajel@gmail.com>
2016-04-08 03:09:10 -0400

    last night's work

--- tests/test_rollparser.py
+++ tests/test_rollparser.py
@@ -17,8 +17,32 @@ class Test_RollParser(unittest.TestCase):
         """ Clean up any changes from the last run. """
         self.rp=None
 
-    def test_random_Roll(self):
+    def test_random_simple_rolls(self):
         """ test a random roll. """
-        self.assertEqual("sure, ok.", self.rp.parse('asd'))
+        for x in range(0,10):
+            result=self.rp.parse("d20")
+            self.assertGreater(21, int(result))
+            self.assertLess(0, int(result))
+
+        for x in range(0,10):
+            result=self.rp.parse("1d20")
+            self.assertGreater(21, int(result))
+            self.assertLess(0, int(result))
+
+        for x in range(0,4):
+            result=self.rp.parse("2d20")
+            self.assertGreater(41, int(result))
+            self.assertLess(0, int(result))
+
+
+    def test_multi_rolls(self):
+        result=self.rp.parse("1d8+3d6+1d4")
+        self.assertGreater(25, int(result))
+        self.assertLess(0, int(result))
+
+    def test_additions(self):
+        result=self.rp.parse("1d6+10 +2 - 4")
+        self.assertGreater(15, int(result))
+        self.assertLess(0, int(result))
 
 
--- tychedice/rollparser/__init__.py
+++ tychedice/rollparser/__init__.py
@@ -5,11 +5,47 @@
 
 import json
 import re
-
+import random
+from pprint import pprint
 class RollParser(object):
     """ RollParser parses roles."""
+
+    pattern = re.compile("""
+        ((\d+)?d(\d+))
+    """, re.VERBOSE)
+
+
+
+#        (
+#        ([^:]+:)?           # Label this roll.
+#        (
+#            (\d+)?d(\d+)        # 4d20
+#            ([HL]\d+|[AD])      # Keep the highest 4, Lowest 4, or use adventage/disadvantage
+#            (\s*[+-]\s*\d\+)?   # + or - a number
+#        [+-]? )+
+#        ;?
+#        )+ 
+
+
+
+
+
+
     def __init__(self):
         """ Create a Parser Object"""
         self.msg= "I'm a parser!"
     def parse(self, string=""):
-        return "sure, ok."
+        m=self.pattern.match(string);
+        pprint(m.group(1))
+        result=self.roll( m.group(2), m.group(1) )
+
+        print "parse %s as %s d %s = %s " % (string, m.group(1), m.group(2), sum(result) )
+        return "2"
+
+    def roll(self, dice, num=1):
+        result=[]
+        if num is None:
+            num=1
+        for i in xrange(int(num)): 
+            result.append( random.randint(1,int(dice)) )
+        return result
