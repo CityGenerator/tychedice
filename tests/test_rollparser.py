41456cf39abd91c12bc5a1d3da3f799031cb3f32
Jesse Morgan (Morgajel) <morgajel@gmail.com>
2016-04-21 22:20:45 -0400

    Working on implementing breakdown text.

--- tests/test_rollparser.py
+++ tests/test_rollparser.py
@@ -18,58 +18,60 @@ class Test_RollParser(unittest.TestCase):
     def tearDown(self):
         """ Clean up any changes from the last run. """
         self.rp=None
-    #def test_isp(self):
-    #    self.assertEqual('    ', self.rp.isp(1))
-    #    self.assertEqual(' '*16, self.rp.isp(4))
-       
 
     def test_parse_modifier(self):
         data='2-33'
 
         modifier = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][0]
         #print "My TopLevel modifier is %s" % str(modifier)
-        result=self.rp.parse_modifier( modifier, data)
+        [result, textstring]=self.rp.parse_modifier( modifier, data)
         self.assertEqual(2, result )
+        self.assertEqual(2, textstring )
        
         modifier = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][4]
         #print "My TopLevel modifier is %s" % str(modifier)
-        result=self.rp.parse_modifier( modifier, data)
+        [result, textstring]=self.rp.parse_modifier( modifier, data)
         self.assertEqual(33, result )
+        self.assertEqual(33, textstring )
 
     def test_parse_dice (self):
         data="d4"
         dice = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][0]
         #print "My TopLevel dice is %s" % str(dice)
     
-        result=self.rp.parse_dice( dice, data)
+        [result, textstring]=self.rp.parse_dice( dice, data)
         self.assertGreater(5, result )
         self.assertLess(0, result )
+        self.assertRegexpMatches(textstring, '\(\d\)' )
 
         
         data="d20"
-        expr=('Dice', 0, 3, [('DiceCount', 0, 0, []), ('DiceType', 1, 3, None)])
-    
-        result=self.rp.parse_dice( expr, data)
+        dice = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][0]
+        [result, textstring]=self.rp.parse_dice( dice, data)
         self.assertGreater(21, result )
         self.assertLess(0, result )
+        self.assertRegexpMatches(textstring, '\(\d\d?\)' )
 
         data="20d6"
-        expr=('Dice', 0, 4, [('DiceCount', 0, 2, []), ('DiceType', 3, 4, None)])
-        result=self.rp.parse_dice( expr, data)
+        dice = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][0]
+        [result, textstring]=self.rp.parse_dice( dice, data)
         self.assertGreater(121, result )
         self.assertLess(19, result )
+        self.assertRegexpMatches(textstring, '\('+'\d, '*19+'\d\)' ) # match 19 \d, followed by the 20th \d.
         
         data="1d20A"
         expr=('Dice', 0, 5, [('DiceCount', 0, 1, []), ('DiceType', 2, 4, None), ('Vantage', 4, 5, None)])
-        result=self.rp.parse_dice( expr, data)
+        [result, textstring]=self.rp.parse_dice( expr, data)
         self.assertGreater(21, result )
         self.assertLess(0, result )
+        self.assertRegexpMatches(textstring, 'Adv\(\d+, \d+\)=\d+' )
 
         data="1d20D"
         expr=('Dice', 0, 5, [('DiceCount', 0, 1, []), ('DiceType', 2, 4, None), ('Vantage', 4, 5, None)])
-        result=self.rp.parse_dice( expr, data)
+        [result, textstring]=self.rp.parse_dice( expr, data)
         self.assertGreater(21, result )
         self.assertLess(0, result )
+        self.assertRegexpMatches(textstring, 'Dis\(\d+, \d+\)=\d+' )
 
 
     def test_parse_op(self):
@@ -77,122 +79,145 @@ class Test_RollParser(unittest.TestCase):
         op = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][2]
         #print "initial op is"
         #pprint(op)
-        result=self.rp.parse_op( op, data)
+        [result, textstring]=self.rp.parse_op( op, data)
         self.assertEqual(1, result )
+        self.assertEqual(textstring, '+' )
+
+        data="3-4"
+        op = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1][3][2]
+        [result, textstring]=self.rp.parse_op( op, data)
+        self.assertEqual(-1, result )
+        self.assertEqual(textstring, '-' )
 
 
     def test_parse_value (self):
         data="3+4"
         value = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
-        result=self.rp.parse_value( value, data)
+        [result, textstring]=self.rp.parse_value( value, data)
         self.assertEqual(7, result )
+        self.assertEqual(textstring, '3  +  4' )
 
         data="10-3"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
-        result=self.rp.parse_value( expr, data)
+        [result, textstring]=self.rp.parse_value( expr, data)
         self.assertEqual(7, result )
+        self.assertEqual(textstring, '10  -  3' )
 
         data="10-3-8"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
         #print "initial valueexpression is"
         #pprint(expr)
-        result=self.rp.parse_value( expr, data)
+        [result, textstring]=self.rp.parse_value( expr, data)
         self.assertEqual(-1, result )
+        self.assertEqual(textstring, '10  -  3  -  8' )
 
         data="1d20+3"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
-        result=self.rp.parse_value( expr, data)
+        [result, textstring]=self.rp.parse_value( expr, data)
         self.assertGreater(24, result )
         self.assertLess(3, result )
+        self.assertRegexpMatches(textstring, '\(\d+\)  \+  3' )
 
         data="1d2+3"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
-        result=self.rp.parse_value( expr, data)
+        [result, textstring]=self.rp.parse_value( expr, data)
         self.assertGreater(6, result )
         self.assertLess(3, result )
+        self.assertRegexpMatches(textstring, '\(\d\)  \+  3' )
 
         data="1d6+1d8"
         value = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3][1]
         #print "My TopLevel Value is %s" % str(value)
-        result=self.rp.parse_value( value, data)
+        [result, textstring]=self.rp.parse_value( value, data)
         self.assertGreater(15, result )
         self.assertLess(1, result )
+        self.assertRegexpMatches(textstring, '\(\d\)  \+  \(\d\)' )
 
     def test_parse_Expression (self):
         data="Attack: 1d20+3+10"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3]
         #print "Testing Expression %s" % str(expr)
-        result=self.rp.parse_expression( expr, data)
+        [result, textstring]=self.rp.parse_expression( expr, data)
         self.assertGreater("Attack: 34", result )
         self.assertLess("Attack: 13", result )
+        self.assertRegexpMatches(textstring, 'Attack:   \(\d+\)  \+  \d  \+  \d' )
 
-        data="damage: 1d2+3"
+        data="Damage: 1d2+3"
         expr = Parser( self.rp.declaration ).parse(data,'AllExpressions')[1][0][3]
-        result=self.rp.parse_expression( expr, data)
-        self.assertGreater("damage: 6", result )
-        self.assertLess("damage: 3", result )
+        [result, textstring]=self.rp.parse_expression( expr, data)
+        self.assertGreater("Damage: 6", result )
+        self.assertLess("Damage: 3", result )
+        self.assertRegexpMatches(textstring, 'Damage:   \(\d+\)  \+  \d' )
 
 
     def test_full_parser(self):
 
-        result=self.rp.parse('2-10')
+        [result, textstring]=self.rp.parse('2-10')
         self.assertEqual(-8, int(result) )
+        self.assertRegexpMatches(textstring, '2  -  10' )
     
-        result=self.rp.parse('d4')
+        [result, textstring]=self.rp.parse('d4')
         self.assertGreater(5, int(result) )
         self.assertLess(0, int(result) )
+        self.assertRegexpMatches(textstring, '\([1-4]\)' )
 
-        result=self.rp.parse('20d6')
+        [result, textstring]=self.rp.parse('20d6')
         self.assertGreater(121, int(result) )
         self.assertLess(19, int(result) )
+        self.assertRegexpMatches(textstring, '\(([1-6], ){19}[1-6]\)' ) #Match 20 total numbers
 
-        result=self.rp.parse('1d20A')
+        [result, textstring]=self.rp.parse('1d20A')
         self.assertGreater(21, int(result) )
         self.assertLess(0, int(result) )
+        self.assertRegexpMatches(textstring, '\(Adv\(\d+, \d+\)=\d+\)' )
 
-        result=self.rp.parse('1d20D')
+        [result, textstring]=self.rp.parse('1d20D')
         self.assertGreater(21, int(result) )
         self.assertLess(0, int(result) )
+        self.assertRegexpMatches(textstring, '\(Dis\(\d+, \d+\)=\d+\)' )
 
-        result=self.rp.parse('2d8+2')
+        [result, textstring]=self.rp.parse('2d8+2')
         self.assertGreater(19, int(result) )
         self.assertLess(3, int(result) )
+        self.assertRegexpMatches(textstring, '\([1-8], [1-8]\)  \+  2' )
 
-        result=self.rp.parse('1d20A+2')
+        [result, textstring]=self.rp.parse('1d20A+2')
         self.assertGreater(23, int(result) )
         self.assertLess(2, int(result) )
+        self.assertRegexpMatches(textstring, '^ \(Adv\(\d+, \d+\)=\d+\)  \+  2$' )
 
-        result=self.rp.parse('1d20D-1')
+        [result, textstring]=self.rp.parse('1d20D-1')
         self.assertGreater(20, int(result) )
         self.assertLess(-1, int(result) )
+        self.assertRegexpMatches(textstring, '^ \(Dis\(\d+, \d+\)=\d+\)  -  1$' )
 
-        result=self.rp.parse('1d8+3d6')
+        [result, textstring]=self.rp.parse('1d8+3d6')
         self.assertGreater(27, int(result) )
         self.assertLess(3, int(result) )
 
-        result=self.rp.parse('1d8-0+3d6-3')
+        [result, textstring]=self.rp.parse('1d8-0+3d6-3')
         self.assertGreater(24, int(result) )
         self.assertLess(0, int(result) )
 
-        result=self.rp.parse('1d6+2d6+1d4')
+        [result, textstring]=self.rp.parse('1d6+2d6+1d4')
         self.assertGreater(23, int(result) )
         self.assertLess(3, int(result) )
 
-        result=self.rp.parse('1d6+2d6+1d4-2')
+        [result, textstring]=self.rp.parse('1d6+2d6+1d4-2')
         self.assertGreater(21, int(result) )
         self.assertLess(1, int(result) )
 
-        result=self.rp.parse('perception check: 1d6A+2')
+        [result, textstring]=self.rp.parse('perception check: 1d6A+2')
         self.assertGreater('perception check: 9', result )
         self.assertLess('perception check: 2', result )
 
-        result=self.rp.parse('save vs. spell: 1d6+1')
+        [result, textstring]=self.rp.parse('save vs. spell: 1d6+1')
         self.assertGreater('save vs. spell: 8', result )
         self.assertLess('save vs. spell: 1', result )
 
-#        result=self.rp.parse('stat roll: 4d6H3')
+#        [result, textstring]=self.rp.parse('stat roll: 4d6H3')
 
-        result=self.rp.parse('Attack: 1d20+4 ; Damage: 1d6+1d4-1')
+        [result, textstring]=self.rp.parse('Attack: 1d20+4 ; Damage: 1d6+1d4-1')
         parts=result.split(' ')
         self.assertEqual('Attack:', parts[0])
         self.assertGreater(25, int(parts[1][0:-1]) )
--- tychedice/rollparser/__init__.py
+++ tychedice/rollparser/__init__.py
@@ -4,7 +4,6 @@
 """ The Roll Parser is the meat of this app."""
 
 import random
-from pprint import pprint
 from simpleparse.parser import Parser
 
 class RollParser(object):
@@ -34,36 +33,41 @@ class RollParser(object):
         parser = Parser(self.declaration)
         expr = parser.parse(data, 'AllExpressions')
         results = []
-        #print "==========================="
-        #print "Data is: '%s" % (data)
+        breakdowns = []
         for expression in expr[1]:
             if expression[0] == "Expression":
-                results.append(str(self.parse_expression(expression[3], data)))
+                [result, breakdown] = self.parse_expression(expression[3], data)
+                results.append(str(result))
+                breakdowns.append(str(breakdown))
 
-        return '; '.join(results)
+        resultstring='; '.join(results)
+        breakdownstring='; '.join(breakdowns)
+        return [resultstring, breakdownstring]
 
 
     def parse_modifier(self, expr, data):
         """ Parse a simple modifier"""
         start = expr[1]
         stop = expr[2]
-        return int(data[start:stop])
+        modifier =  int(data[start:stop])
+        return [modifier, modifier]
 
     def parse_op(self, expr, data):
         """ Parse an operator, which is limited to returning a +1 or -1 multiplier"""
         start = expr[1]
         stop = expr[2]
-        if data[start:stop] == '+':
-            return 1
+        operator = data[start:stop]
+        if operator == '+':
+            return [1, operator]
         else:
-            return -1
+            return [-1, operator]
 
     def parse_dice(self, expr, data):
         """ Parse a dice structure"""
         result = 0
         rolled = []
         dicecount = 1
-
+        breakdowns=[]
         start = expr[3][0][1]
         stop = expr[3][0][2]
         if data[start:stop] != '':
@@ -73,6 +77,7 @@ class RollParser(object):
         stop = expr[3][1][2]
         for _ in range(0, dicecount):
             roll = 0
+            breakdown = ''
             if expr[3][-1][0] == 'Vantage':
 
                 vstart = expr[3][-1][1]
@@ -81,43 +86,61 @@ class RollParser(object):
                     a = random.randint(1, int(data[start:stop]))
                     b = random.randint(1, int(data[start:stop]))
                     roll = max(a, b)
+                    breakdown='Adv('+str(a)+', '+str(b)+')='+str(roll)
                 else:
                     a = random.randint(1, int(data[start:stop]))
                     b = random.randint(1, int(data[start:stop]))
                     roll = min(a, b)
+                    breakdown='Dis('+str(a)+', '+str(b)+')='+str(roll)
             else:
                 roll = random.randint(1, int(data[start:stop]))
+                breakdown=roll
 
             rolled.append(roll)
+            breakdowns.append(str(breakdown))
             result += roll
 
-        return result
+        breakdownstring='('+ ', '.join(breakdowns)+')'
+        return [result, breakdownstring]
 
     def parse_value(self, expr, data):
         """ Parse a list of values which may include modifiers, operatiors and dice."""
         result = 0
+        textresults=[]
         last_op = 1
+        last_op_text = ''
         for i in expr[3]:
+            textstring=''
             if i[0] == 'Modifier':
-                result = result + last_op * self.parse_modifier(i, data)
+                [modifier, textstring]=self.parse_modifier(i, data)
+                result = result + last_op * modifier
             elif i[0] == 'Operator':
-                last_op = self.parse_op(i, data)
+                [last_op, textstring] = self.parse_op(i, data)
             elif i[0] == 'Dice':
-                result = result + last_op * self.parse_dice(i, data)
-        return result
+                [dice, textstring] = self.parse_dice(i, data)
+                result = result + last_op * dice
+            textresults.append(str(textstring))
+        finaltext= ' '.join(textresults)
+        return [result,finaltext]
 
     def parse_expression(self, expr, data):
         """ Parse a full expression with optional label."""
         result = ""
+        textstrings=[]
         for i in expr:
+            textstring=''
             if i[0] == 'Label':
                 start = i[1]
                 stop = i[2]
-                result += data[start:stop] + " "
+                labeltext=data[start:stop] + " "
+                result += labeltext
+                textstring = labeltext
             elif i[0] == 'Value':
                 start = i[1]
                 stop = i[2]
-                result += str(self.parse_value(i, data))
-
-        return result
+                [value, textstring]=self.parse_value(i, data)
+                result += str(value)
+            textstrings.append(textstring)
+        finaltext=' '.join(textstrings)
+        return [result, finaltext]
 
